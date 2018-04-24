a28affb00adf11120b917a9ce3b8db452330d0e3
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2018-04-24 11:29:10 +0200

    remove autostart from singleNotebookapp config

--- greekInscriptions/config/jupyter_notebook_config.py
+++ greekInscriptions/config/jupyter_notebook_config.py
@@ -1,12 +1,12 @@
-c = get_config()
+#c = get_config()
 
-c.SingleUserNotebookApp.file_to_run = '/home/jovyan/data/index.ipynb'
-c.NotebookApp.iopub_data_rate_limit=1.0e10
-
-c.NotebookApp.allow_origin = '*'
-
-c.NotebookApp.tornado_settings = {
-    'headers': {
-        'Content-Security-Policy': "frame-ancestor 'self' https://notebooks.mpiwg-berlin.mpg.de; child-src *"
-    }
-}
+#c.SingleUserNotebookApp.file_to_run = '/home/jovyan/data/index.ipynb'
+# c.NotebookApp.iopub_data_rate_limit=1.0e10
+#
+# c.NotebookApp.allow_origin = '*'
+#
+# c.NotebookApp.tornado_settings = {
+#     'headers': {
+#         'Content-Security-Policy': "frame-ancestor 'self' https://notebooks.mpiwg-berlin.mpg.de; child-src *"
+#     }
+# }
